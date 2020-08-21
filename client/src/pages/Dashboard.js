2b1231547a51c1f9876537d35a447a768d9aa319
Puneet <birdipuneet@gmail.com>
2020-08-21 13:34:42 -0400

    Created modal component

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -3,14 +3,14 @@ import { Switch, Route } from 'react-router-dom';
 import Overview from './Overview';
 //SUBPAGES============
 import Sidebar from '../components/layout/Sidebar';
+import TransactionInput from '../components/modals/TransactionInput';
 
 const Dashboard = (props) => {
   return (
     <section className='container static flex-1 h-full max-h-full min-w-full flex'>
       <Sidebar />
-      <Switch>
-        <Route exact path={`/`} component={Overview} />
-      </Switch>
+      <Overview />
+      <TransactionInput />
     </section>
   );
 };
